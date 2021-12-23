33231175090289897f9e06f050a89ad2414f2f09
Christa Bella Kaneza <christakabella@gmail.com>
2021-12-23 02:37:24 +0200

    Finish Coding challenge #1

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
-let country='Burundi'
-let population= 12000000
-let continent='Afrique'
+/*let country = 'Burundi';
+let population = 12000000;
+let continent = 'Afrique';
 
 console.log(country);
 console.log(population);
 console.log(continent);
 
+*/
 
-console.log('country');
+
+//Coding Challenge #1
+//////// DATA 1
+
+/*const massJanvier = 70;
+const heightJanvier = 1.60;
+const massChrista = 90;
+const heightChrista = 1.90;
+
+const BMIJanvier = massJanvier / heightJanvier ** 2;
+const BMIChrista = massChrista / (heightChrista ** heightChrista);
+const markHigherBMI = BMIJanvier > BMIChrista;
+
+console.log (BMIJanvier, BMIChrista, markHigherBMI);*/
+
+
+///////DATA 2
+const massJanvier = 90;
+const heightJanvier = 1.80;
+const massChrista = 85;
+const heightChrista = 1.70;
+
+ const BMIJanvier = massJanvier / heightJanvier ** 2;
+ const BMIChrista = massChrista / (heightChrista ** heightChrista);
+ const markHigherBMI = BMIJanvier > BMIChrista;
+
+console.log(BMIJanvier, BMIChrista, markHigherBMI);
+
+
